f61a9356a0ea6becb2030cf149089929bb60863b
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-20 13:52:51 -0400

    Implemented 3 tests for adding empty string, 1 number, and 2 numbers

---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(int num)
+        {
+            if (num == ' ')    
+                return 0;
+            return num;
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    /* The method can take 0, 
+     * 1 or 2 numbers, and will return their sum 
+     * (for an empty string it will return 0) 
+     * for example “” or “1” or “1,2”
+    */
+
+    [TestFixture]
+    public class Tests
+    {
+        Calculator calc = new Calculator();
+
+        [Test]
+        public void EmptyString_ReturnZero()
+        {
+            calc.Add(' ');
+
+            Assert.AreEqual(0, 0);
+        }
+
+        [Test]
+        public void OneNumber_ReturnsNumber()
+        {
+            calc.Add(3);
+            Assert.AreEqual(3, 3);
+        }
+
+        [Test]
+        public void TwoNumbers_ReturnsSum()
+        {
+            calc.Add(2, 3);
+            Assert.AreEqual(5, 5);
+        }
+    }
+}

9c87832b739087d6376f399fc6c67eedd8cc7aec
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-17 14:12:33 -0400

    Tests added for greeting two people and 3 people with both lower and uppercase names

---
+++ Greetings/GreetingMaker/Greeting.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingMaker
+{
+    public class Greeting
+    {
+        public string GreetPerson(string name)
+        {
+            if (name == null)
+                name = "my friend";
+            foreach (char letter in name)
+            {
+                if (Char.IsLower(letter))
+                    return "Hello, " + name;
+            }
+            return "HELLO, " + name;
+        }
+
+        public string GreetPerson(string[] names)
+        {
+            string startingString = "Hello, ";
+            string allCaps = "AND HELLO ";
+            bool hasCaps = false;
+            StringBuilder newString = new StringBuilder(startingString);
+            StringBuilder capString = new StringBuilder(allCaps);
+
+            if (names.Length > 2)
+            {
+                if (names[0] == names[0].ToUpper() || names[1] == names[1].ToUpper() || names[2] == names[2].ToUpper())
+                {
+                    hasCaps = true;
+                }
+
+                int lastName = names.Length - 1;
+                for (var i = 0; i < names.Length - 1; i++)
+                {
+                    Console.WriteLine(i);
+                    if (hasCaps)
+                    {
+                        if (names[i] == names[i].ToUpper())
+                        {
+                            capString.Append(names[i] + "!");
+                        }
+
+                        else
+                        {
+                            newString.Append(names[i] + " and ");
+                        }
+
+                        hasCaps = true;
+                    }
+
+                    else if (names[i] != names[i].ToUpper() || names[i + 1] != names[i + 1].ToUpper() || names[i + 2] != names[i + 2].ToUpper())
+                    {
+                        newString.Append(names[i] + ", " + names[i + 1] + ", ");
+                    }
+                }
+
+                if (hasCaps)
+                {
+                    newString.Append(names[lastName] + ". ");
+                    return newString.ToString() + capString.ToString();
+                }
+
+                else
+                {
+                    newString.Append("and " + names[lastName] + ".");
+                    return newString.ToString();
+                }
+
+            }
+
+            return "Hello, " + names[0] + " and " + names[1];
+        }
+    }
+}
---
+++ Greetings/GreetingMakerTests/GreetingTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using GreetingMaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingMakerTests
+{
+    [TestFixture]
+    public class GreetingTests
+    {
+        Greeting greet = new Greeting();
+        string name;
+        string[] names;
+
+        [Test]
+        public void GreetASinglePerson_ReturnString()
+        {
+            name = "Bob";
+            string expected = "Hello, Bob";
+            string actual = greet.GreetPerson(name);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNameIsNull_ReturnHelloMyFriend()
+        {
+            name = null;
+            var expected = "Hello, my friend";
+            var actual = greet.GreetPerson(name);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfNameIsAllCapsReturnHELLO()
+        {
+            name = "JERRY";
+            var expected = "HELLO, JERRY";
+            var actual = greet.GreetPerson(name);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfNameIsAnArray_GreetBothPeople()
+        {
+            names = new string[] { "Todd", "Sarah" };
+            var expected = "Hello, Todd and Sarah";
+            var actual = greet.GreetPerson(names);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfThreeNames_ReturnGreetingWithCommas()
+        {
+            names = new string[] { "Todd", "Sarah", "Bob" };
+            var expected = "Hello, Todd, Sarah, and Bob.";
+            var actual = greet.GreetPerson(names);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IfArrayHasMixedRegularAndAllUppercaseNamesReturnAllUppercaseSecondSentence()
+        {
+            names = new string[] { "Todd", "SARAH", "Bob" };
+            var expected = "Hello, Todd and Bob. AND HELLO SARAH!";
+            var actual = greet.GreetPerson(names);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
